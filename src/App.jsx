8e578753a1674b8343b241fa3268f7e1ef75539b
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-04 14:34:04 +0100

    c
    Adds Product page routing to the application
    
    Introduces routing for the Product page within the application.
    
    This change ensures users can navigate to the Product page seamlessly from the main application interface.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Product from "./pages/Product";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="product" element={<Product />} />
