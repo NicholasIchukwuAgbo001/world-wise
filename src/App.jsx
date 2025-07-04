7d83ade9abfa1a59c51e6715349a7c7f25aab1cd
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-04 13:49:57 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      Worldwise
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      WorldWise
+    </div>
+  )
+}
+
+export default Home
